0d00bf8f3005dc25e04ea5a85315e834a55158ee
mochineko <mochinekoo0113@gmail.com>
2025-11-15 21:48:22 +0900

    [add] スコアボードを追加

--- src/main/java/mochineko/coreattack_pvp/Main.java
+++ src/main/java/mochineko/coreattack_pvp/Main.java
@@ -2,6 +2,9 @@ package mochineko.coreattack_pvp;
 
 import mochineko.coreattack_pvp.command.GameResetCommand;
 import mochineko.coreattack_pvp.command.GameStartCommand;
+import mochineko.coreattack_pvp.manager.ScoreboardManager;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
@@ -12,6 +15,11 @@ public final class Main extends JavaPlugin {
 
         getCommand("game_start").setExecutor(new GameStartCommand());
         getCommand("game_reset").setExecutor(new GameResetCommand());
+
+        //スコアボード
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            ScoreboardManager.getInstance(player.getUniqueId()).setScoreboard();
+        }
 
         getLogger().info("プラグインが有効になりました。");
     }
