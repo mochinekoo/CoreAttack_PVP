2465ebc3d6aa85c043c8c61f4c2885ce0eee4dbf
mochineko <mochinekoo0113@gmail.com>
2025-11-16 15:53:51 +0900

    [add] ゲームを開始できるようになりました!

--- src/main/java/mochineko/coreattack_pvp/command/GameStartCommand.java
+++ src/main/java/mochineko/coreattack_pvp/command/GameStartCommand.java
@@ -1,5 +1,6 @@
 package mochineko.coreattack_pvp.command;
 
+import mochineko.coreattack_pvp.manager.GameManager;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -10,7 +11,8 @@ public class GameStartCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender send, @NotNull Command cmd, @NotNull String s, @NotNull String[] args) {
         if (cmd.getName().equalsIgnoreCase("game_start")) {
-
+            GameManager manager = GameManager.getInstance();
+            manager.startGame();
         }
         return false;
     }
--- src/main/java/mochineko/coreattack_pvp/manager/GameManager.java
+++ src/main/java/mochineko/coreattack_pvp/manager/GameManager.java
@@ -1,6 +1,10 @@
 package mochineko.coreattack_pvp.manager;
 
+import mochineko.coreattack_pvp.Main;
 import mochineko.coreattack_pvp.status.GameStatus;
+import mochineko.coreattack_pvp.utility.TextUtil;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
 
 public class GameManager {
 
@@ -9,11 +13,36 @@ public class GameManager {
 
     //ゲーム
     private GameStatus status;
+    private BukkitTask task;
     private int time;
 
     private GameManager() {
         this.status = GameStatus.WAITING;
         this.time = 60*30;
+    }
+
+    public void startGame() {
+        if (task == null) {
+            this.task = new BukkitRunnable() {
+                int countTime = 10;
+                @Override
+                public void run() {
+                    if (status == GameStatus.WAITING || status == GameStatus.COUNTING) {
+                        if (countTime > 0) {
+                            TextUtil.sendInfoMessage("ゲーム開始まであと" + countTime + "秒");
+                            countTime--;
+                        }
+                        else {
+                            TextUtil.sendInfoMessage("ゲーム開始!!");
+                            status = GameStatus.RUNNING;
+                        }
+                    }
+                    else if (status == GameStatus.RUNNING) {
+
+                    }
+                }
+            }.runTaskTimer(Main.getPlugin(Main.class), 0L, 20L);
+        }
     }
 
     public int getTime() {
