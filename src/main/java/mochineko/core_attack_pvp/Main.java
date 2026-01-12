7535b1ad5334ad72d979b8ae1f9963a367f42e95
mochineko <mochinekoo0113@gmail.com>
2026-01-12 22:06:20 +0900

    [add] configを追加しました

--- src/main/java/mochineko/core_attack_pvp/Main.java
+++ src/main/java/mochineko/core_attack_pvp/Main.java
@@ -1,13 +1,25 @@
 package mochineko.core_attack_pvp;
 
+import mochineko.core_attack_pvp.command.GameStartCommand;
+import mochineko.core_attack_pvp.command.TeamCommand;
+import mochineko.core_attack_pvp.listener.BlockBreakListener;
+import mochineko.core_attack_pvp.listener.PlayerChatListener;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        saveDefaultConfig();
 
+        // Plugin startup logic
+        PluginManager plm = getServer().getPluginManager();
+        plm.registerEvents(new BlockBreakListener(), this);
+        plm.registerEvents(new PlayerChatListener(), this);
+
+        getCommand("game_start").setExecutor(new GameStartCommand());
+        getCommand("game_team").setExecutor(new TeamCommand());
     }
 
     @Override
