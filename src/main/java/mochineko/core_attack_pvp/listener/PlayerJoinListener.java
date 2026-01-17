4d7c925c076e6239b3a93a64c53cdfba7785fd02
mochineko <mochinekoo0113@gmail.com>
2026-01-17 21:36:28 +0900

    [add] ログイン時にロビーにスポーンするようになりました

--- src/main/java/mochineko/core_attack_pvp/listener/PlayerJoinListener.java
+++ src/main/java/mochineko/core_attack_pvp/listener/PlayerJoinListener.java
@@ -1,6 +1,8 @@
 package mochineko.core_attack_pvp.listener;
 
+import mochineko.core_attack_pvp.manager.ConfigManager;
 import mochineko.core_attack_pvp.manager.ScoreboardManager;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,7 +13,12 @@ public class PlayerJoinListener implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
+        ConfigManager config = ConfigManager.getInstance();
+        Location lobby = config.getLobby();
         ScoreboardManager scoreboardManager = ScoreboardManager.getInstance(player.getUniqueId());
         scoreboardManager.setScoreboard();
+        if (lobby != null) {
+            player.teleport(lobby);
+        }
     }
 }
